aea721fd08fce105926864da8bddd2e1bfc3ac94
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2024-02-29 11:52:42 +0000

    set up

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
-  bgColor: "#353b48",
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
   textColor: "#f5f6fa",
-  accentColor: "#fbc531",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
 };
