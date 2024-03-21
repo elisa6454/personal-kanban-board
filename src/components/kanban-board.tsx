6ef395deec0e899b462cae9b838f7adbaacfdb87
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2024-03-21 14:49:50 +0000

    edit navigation icons interval
    The icon was cut off so only half of it was visible, so I fixed the spacing

--- src/components/kanban-board.tsx
+++ src/components/kanban-board.tsx
@@ -19,19 +19,19 @@ const Trash = styled.div`
   justify-content: center;
   position: fixed;
   top: -3.75rem;
-  left: calc(50vw - 3.75rem);
+  left: calc(50vw - 3.5rem);
   width: 7.5rem;
   height: 3.75rem;
   border-radius: 0 0 100rem 100rem;
   background-color: tomato;
   box-shadow: -0.1rem 0 0.4rem rgb(210 77 77 / 15%);
-  font-size: 2.5rem;
+  font-size: 10px;
   z-index: 5;
   transition: transform 0.3s;
 
   & > div {
     margin-bottom: 0.5rem;
-    color: rgba(0, 0, 0, 0.5);
+    color: rgba(, 0, 0, 0.5);
   }
 `;
 const GlobalStyle = createGlobalStyle`
@@ -159,7 +159,7 @@ const Button = styled.button`
 const Navigation = styled.nav`
   display: flex;
   position: fixed;
-  padding: 2.5rem 3rem;
+  padding: 2.5rem 7rem;
   align-items: center;
   justify-content: space-between;
   width: 100vw;
@@ -373,17 +373,15 @@ export default function KanbanBoard() {
             <div>
               <Trash ref={provided.innerRef} {...provided.droppableProps}>
                 <svg
-                  fill="none"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
                   xmlns="http://www.w3.org/2000/svg"
                   aria-hidden="true"
                 >
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z"
                   />
                 </svg>
               </Trash>
