eb22f97dad6284500ec5db69b38232432b7a25b8
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2024-04-02 17:44:16 +0100

    edit css trash all button

--- src/components/trash-board.tsx
+++ src/components/trash-board.tsx
@@ -13,13 +13,25 @@ const Title = styled.h1`
 `;
 
 const Button = styled.button`
+  display: flex;
+  align-items: center;
   background-color: #f44336;
-  color: white;
-  padding: 3px 5px;
   border: none;
-  border-radius: 5px;
+  border-radius: 8px;
+  padding: 12px 16px;
+  font-size: 18px;
+  color: white;
   cursor: pointer;
-  font-size: 16px;
+  svg {
+    width: 30px;
+    margin-right: 7px;
+  }
+  margin-left: auto;
+  transition: background-color 0.3s, color 0.3s;
+  &: hover {
+    background-color: white;
+    color: #f44336;
+  }
 `;
 
 export default function TrashBoard() {
@@ -40,10 +52,10 @@ export default function TrashBoard() {
           <path
             clipRule="evenodd"
             fillRule="evenodd"
-            d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z"
+            d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
           />
         </svg>
-        clear all trash
+        Clear all trash
       </Button>
     </ThemeProvider>
   );
